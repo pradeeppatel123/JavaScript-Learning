f3adb1d3e17cba713a01837d2e8d591234109270
Pradeep Patel <111377568+pradeeppatel123@users.noreply.github.com>
2023-11-26 03:10:27 +0000

    devchanges

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const accountId = 144553;
+let accountEmail = "pradeepsingh@gmail.com";
+var accountPassword =  "12345";
+accountCity = "jaipur";
+// accountId =2; // Not allowed
+accountEmail = "pp06@gmail.com"
+accountPassword = "2123"
+accountCity = "Bangaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
