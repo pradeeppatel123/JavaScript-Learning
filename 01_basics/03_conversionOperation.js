0c40437a4e08025873016b1fb6dbaff0e82040de
Pradeep Patel <111377568+pradeeppatel123@users.noreply.github.com>
2023-11-26 04:39:06 +0000

    changes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); 
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn  = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Pradeep" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
