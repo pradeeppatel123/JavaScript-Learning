618b68e5c2ea06b38b972031a93fa32073e4fbd5
Pradeep Patel <111377568+pradeeppatel123@users.noreply.github.com>
2023-11-26 05:07:39 +0000

    changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,23 @@ let booleanIsLoggedIn  = Boolean(isLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+// ********************** Operations *******************
+
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+3
