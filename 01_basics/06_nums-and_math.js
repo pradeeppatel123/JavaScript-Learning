5f8a3325bc1e07a5f99f3d52a1bba2f555762f27
Pradeep Patel <111377568+pradeeppatel123@users.noreply.github.com>
2023-12-30 08:26:19 +0000

    Math and num

---
+++ 01_basics/06_nums-and_math.js
@@ -0,0 +1,30 @@
+// ++++++++++++++++++ Nums ++++++++++++++++++++++++
+
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(Number.MAX_VALUE);
+
+// +++++++++++++++++++++++++ Maths ++++++++++++++++++++++
+
+// console.log(Math.abs(4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
