b895098e665b0f1174bab137d55fee8f7a4b42f5
Pradeep Patel <111377568+pradeeppatel123@users.noreply.github.com>
2023-12-30 04:52:12 +0000

    changes

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const name  = "Pradeep"
+const repoCount  = 50
+console.log(`Hello My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName  = new String('Pradeep-singh')
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('P'));
+// const newString  = gameName.substring(0,4);
+// console.log(newString);
+// const anotherString = gameName.slice(-7,2);
+// console.log(anotherString);
+
+// const newStringOne  = "    Pradeep.    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url  = "https://pradeep.com/pradeep%20patel"
+console.log(url.replace('%20','-'));
+console.log(url.includes('pradeep'));
+console.log(gameName.split('-'));
--- 01_basics/datatypes_Summary.js
+++ 01_basics/datatypes_Summary.js
@@ -5,7 +5,31 @@ const id = Symbol('123')
 const anotherId = Symbol('123')
 console.log(id === anotherId);
 
-// Reference Type or Non-Primitive
+// Reference or Non-Primitive
 
 // Array, Objects, Function
 console.log(typeof anotherId);
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap(Non-Primitive)
+
+let myYoutubename  = "pradeeppatel.com";
+let anothername  = myYoutubename;
+anothername  = "baldaoopatel.com"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne  = {
+    email :"user@gmail.com",
+    upi:"user@ybl"
+}
+let userTwo  = userOne ;
+
+userTwo.email = "pradeep@gmail.com"
+
+console.log(userOne.email);
+
+console.log(userTwo.email);
