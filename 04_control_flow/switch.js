8e0c2d13dc52b613c275601f1b149ef2c7480919
Pradeep Patel <111377568+pradeeppatel123@users.noreply.github.com>
2024-01-03 13:46:55 +0000

    Switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+const month = 3;
+switch (month) {
+  case 1:
+    console.log("Jan");
+    break;
+  case 2:
+    console.log("Feb");
+    break;
+  case 3:
+    console.log("Mar");
+    break;
+  case 4:
+    console.log("Apri");
+    break;
+
+  default:
+    console.log("Default case Match");
+    break;
+}
