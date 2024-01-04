b41322dab978b97e903715cfbc1a259c7d5c3d97
Pradeep Patel <111377568+pradeeppatel123@users.noreply.github.com>
2024-01-04 14:21:23 +0000

    chanes

--- Projects/02_BMIConvertor/script.js
+++ Projects/02_BMIConvertor/script.js
@@ -1,21 +1,23 @@
 const form = document.querySelector("form");
+
 // This use case will give you empty value
 //  const height = parseInt(document.querySelector('#height').value);
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const height = parseInt(document.querySelector("#height").value);
+
   const weight = parseInt(document.querySelector("#weight").value);
+
   const results = document.querySelector("#results");
 
   if (height === "" || height < 0 || isNaN(height)) {
-    results.innerHTML = `Please give a valid height ${height}`;
+    results.innerHTML = `Please Enter the valid height ${height}`;
   } else if (weight === "" || weight < 0 || isNaN(weight)) {
-    results.innerHTML = `Please give a valid weight ${weight}`;
-  }
-  else{
-    const BMI = (weight/((height*height)/10000)).toFixed(2);
-    // show the results
-    results.innerHTML =`<span>${BMI}</span>`
+    results.innerHTML = `Please Enter the valid weight ${weight}`;
+  } else {
+    const BMI = (weight / ((height * height) / 10000)).toFixed(2);
+    results.innerHTML = `<span>${BMI}</span>`;
   }
 });
