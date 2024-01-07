962a477f07daf4ac9ea582a519f09048857ee9a3
pradeeppatel123 <pp0608105@gmail.com>
2024-01-07 11:49:01 +0530

    class

---
+++ 10_classes_and_oop/myClasses.js
@@ -0,0 +1,38 @@
+// ES6
+
+// class User {
+//   constructor(username, email, password) {
+//     this.username = username;
+//     this.email = email;
+//     this.password = password;
+//     }
+//     encryptPassword() {
+//         return `${this.password}abc`
+//     }
+//     changeUserName() {
+//         return`${this.username.toUpperCase()}`
+//     }
+// }
+
+// const chai = new User("chai","chai@gmail.com","123")
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUserName());
+
+// behind the seen
+
+function User(username, email, password) {
+  this.username = username;
+  this.email = email;
+  this.password = password;
+}
+User.prototype.encryptPassword = function () {
+  return `${this.password}abc`;
+};
+User.prototype.changeUserName = function () {
+  return `${this.username.toUpperCase()}`;
+};
+
+const tea = new User("Tea", "tea@gmail.com", "2345");
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUserName());
