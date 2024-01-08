a21ce81ed4b479f02324ed4d83b3584a20b178b1
pradeeppatel123 <pp0608105@gmail.com>
2024-01-08 10:26:01 +0530

    toggle mode

---
+++ 07_projects/toggle/script.js
@@ -0,0 +1,28 @@
+let toggleBtn = document.getElementById("toggle-btn");
+let body = document.body;
+let darkMode = localStorage.getItem("dark-mode");
+
+const enableDarkMode = () => {
+  toggleBtn.classList.replace("fa-sun", "fa-moon");
+  body.classList.add("dark");
+  localStorage.setItem("dark-mode", "enabled");
+};
+
+const disableDarkMode = () => {
+  toggleBtn.classList.replace("fa-moon", "fa-sun");
+  body.classList.remove("dark");
+  localStorage.setItem("dark-mode", "disabled");
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+toggleBtn.onclick = (e) => {
+  darkMode = localStorage.getItem("dark-mode");
+  if (darkMode === "disabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+};
