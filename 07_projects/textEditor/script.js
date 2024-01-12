0954432d6843621d9c0920011f3295f85a331dad
pradeeppatel123 <pp0608105@gmail.com>
2024-01-12 21:05:35 +0530

    changes

--- 07_projects/textEditor/script.js
+++ 07_projects/textEditor/script.js
@@ -1,22 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
   const inputField = document.getElementById("input-field");
+  const outputField = document.getElementById("output-field");
   const upperCase = document.querySelector(".uppercase");
   const lowerCase = document.querySelector(".lowercase");
   const capitalize = document.querySelector(".capitalize");
   const bold = document.querySelector(".bold");
   const italic = document.querySelector(".italic");
-  const underline = document.querySelector(".underline");
-  const outputField = document.querySelector("#output-field");
+  const underLine = document.querySelector(".underline");
 
   upperCase.addEventListener("click", function () {
     outputField.textContent = inputField.value.toUpperCase();
   });
+
   lowerCase.addEventListener("click", function () {
     outputField.textContent = inputField.value.toLowerCase();
   });
 
   capitalize.addEventListener("click", function () {
-    outputField.textContent = capitalizeInputValue(inputField.value);
+    outputField.textContent = capitalizeWord(inputField.value);
   });
 
   bold.addEventListener("click", function () {
@@ -26,12 +27,11 @@ document.addEventListener("DOMContentLoaded", function () {
   italic.addEventListener("click", function () {
     outputField.innerHTML = `<em>${inputField.value}</em>`;
   });
-
-  underline.addEventListener("click", function () {
+  underLine.addEventListener("click", function () {
     outputField.innerHTML = `<u>${inputField.value}</u>`;
   });
 
-  function capitalizeInputValue(str) {
+  function capitalizeWord(str) {
     return str.replace(/\b\w/g, function (char) {
       return char.toUpperCase();
     });
